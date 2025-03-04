e08868ccd35fbb58a23674331c55fd27710cf28b
lenin <lenin.vn.hn@gmail.com>
2025-03-04 09:22:54 +0700

    First commit

---
+++ src/main/java/com/lenin/hotel/authentication/request/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.lenin.hotel.authentication.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank(message = "Username cannot be empty")
+//    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, max = 30, message = "Password must be between 8 and 30 characters")
+//    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=.*[0-9]).{8,}$",
+//            message = "Password must contain at least one uppercase letter, one special character (@#$%^&+=!), and one number")
+    private String password;
+
+}
---
+++ src/main/java/com/lenin/hotel/authentication/service/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.lenin.hotel.authentication.service;
+
+import com.lenin.hotel.authentication.model.User;
+import com.lenin.hotel.authentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService  implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User Not Found with username: " + username);
+        }
+        return UserDetailsImpl.build(user);
+    }
+}
