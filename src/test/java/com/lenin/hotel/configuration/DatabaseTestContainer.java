c2d482ac15eada68898a2a1c002a4a0b8b9a0530
lenin <lenin.vn.hn@gmail.com>
2025-04-22 14:36:29 +0700

    add testContainer

---
+++ src/test/java/com/lenin/hotel/configuration/DatabaseTestContainer.java
@@ -0,0 +1,23 @@
+package com.lenin.hotel.configuration;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration
+public class DatabaseTestContainer {
+    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:17"))
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    static {
+        postgresContainer.start();  // Khởi động container ngay khi lớp này được nạp
+    }
+
+    @Bean
+    public PostgreSQLContainer<?> postgresqlContainer() {
+        return postgresContainer;
+    }
+}
